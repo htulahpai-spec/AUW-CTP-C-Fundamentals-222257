d9ab5bb12f09a335e74a03887012b21ba80db70b
htulahpai-spec <htu.lahpai@auw.edu.bd>
2026-02-17 15:39:01 +0600

    Add files via upload

---
+++ q44_222257.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    int count = 0;
+
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d",&arr[i]);
+
+        if(arr[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    printf("Positive Numbers Count = %d",count);
+
+    return 0;
+}
