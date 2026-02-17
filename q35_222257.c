cdfd630447d107078260296ba7b049dd0ca78179
htulahpai-spec <htu.lahpai@auw.edu.bd>
2026-02-17 15:34:16 +0600

    Add files via upload

---
+++ q35_222257.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+    scanf("%d",&year);
+
+    if(year<=0)
+        printf("Invalid Year");
+    else if(year%400==0)
+        printf("Leap Year");
+    else if(year%100==0)
+        printf("Not Leap Year");
+    else if(year%4==0)
+        printf("Leap Year");
+    else
+        printf("Not Leap Year");
+
+    return 0;
+}
